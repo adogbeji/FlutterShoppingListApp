fc8e8a03362be2f295cf03738729c80d93f9f727
adogbeji <baka_1993@yahoo.co.uk>
2024-08-19 10:34:38 +0100

    Add starting code for NewItem widget

---
+++ lib/widgets/new_item.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() => _NewItemState();
+}
+
+class _NewItemState extends State<NewItem> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('New Item Screen'),
+      ),
+    );
+  }
+}
