95c2996f2ece625812fbb29719c9187f44aceece
adogbeji <baka_1993@yahoo.co.uk>
2024-08-20 10:57:41 +0100

    Set validator parameter in NewItem widget

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -14,11 +14,21 @@ class _NewItemState extends State<NewItem> {
       appBar: AppBar(
         title: const Text('Add a new item'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(12),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
         child: Form(
           child: Column(
-            children: [],
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text('Name'),
+                ),
+                validator: (value) {
+                  return 'Demo...';
+                },
+              ),
+            ],
           ),
         ),
       ),
