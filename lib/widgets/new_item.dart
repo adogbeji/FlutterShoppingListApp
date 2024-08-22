50c4f4c8b96672a1421ee92cd4289181221b29df
adogbeji <baka_1993@yahoo.co.uk>
2024-08-22 19:35:36 +0100

    Set validator parameter on second TextFormField in NewItem widget

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -28,16 +28,15 @@ class _NewItemState extends State<NewItem> {
                   label: Text('Name'),
                 ),
                 validator: (value) {
-                  if (value == null || 
-                      value.isEmpty || 
-                      value.trim().length <= 1 || 
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
                       value.trim().length > 50) {
-                        return 'Must be between 1 and 50 characters long!';
-                      }
-                      return null;
+                    return 'Must be between 1 and 50 characters long!';
+                  }
+                  return null;
                 },
               ),
-
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
@@ -47,16 +46,26 @@ class _NewItemState extends State<NewItem> {
                         label: Text('Quantity'),
                       ),
                       initialValue: '1',
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be between 1 and 50 characters long!';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                  const SizedBox(width: 8,),
+                  const SizedBox(
+                    width: 8,
+                  ),
                   Expanded(
-                    child: DropdownButtonFormField(
-                      items: [
-                        for (final category in categories.entries)
-                          DropdownMenuItem(
-                            value: category.value,
-                            child: Row(
+                    child: DropdownButtonFormField(items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                          value: category.value,
+                          child: Row(
                             children: [
                               Container(
                                 width: 16,
@@ -66,16 +75,13 @@ class _NewItemState extends State<NewItem> {
                               const SizedBox(width: 6),
                               Text(category.value.title),
                             ],
-                          ),),
-                      ], 
-                      onChanged: (value) {}
-                    ),
+                          ),
+                        ),
+                    ], onChanged: (value) {}),
                   ),
                 ],
               ),
-
               const SizedBox(height: 12),
-
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
