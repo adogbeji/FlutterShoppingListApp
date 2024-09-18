6d2ed0cecf23befb2a80acb5b71a29a25f491026
adogbeji <baka_1993@yahoo.co.uk>
2024-09-18 10:49:17 +0100

    Handle errors in GroceryList widget

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -19,6 +19,7 @@ class GroceryList extends StatefulWidget {
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
   var _isLoading = true;
+  String? _error;
 
   @override
   void initState() {
@@ -30,6 +31,13 @@ class _GroceryListState extends State<GroceryList> {
   void _loadItems() async {
     final url = Uri.https('shopping-list-9ea6f-default-rtdb.firebaseio.com', 'shopping-list.json');
     final response = await http.get(url);  // Sends GET request to Firebase
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _error = 'Failed to fetch data. Please try again later.';
+      });
+    }
+    
     // print(response);
     final Map<String, dynamic>listData = json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
@@ -109,6 +117,12 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ),
       );    
+    }
+
+    if (_error != null) {
+      content = Center(
+        child: Text(_error!),
+      );
     }
 
     return Scaffold(
